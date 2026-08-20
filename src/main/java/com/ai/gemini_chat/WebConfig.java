12a69c7382c2fda2ddd2a5a882945c50b78f2eea
Saurabh Yadav <saurabh17again@gmail.com>
2026-08-20 19:50:15 +0530

    Fix cors for frontend deployed

--- src/main/java/com/ai/gemini_chat/WebConfig.java
+++ src/main/java/com/ai/gemini_chat/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173/")
                 .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+//                .allowedHeaders("*")
                 .allowCredentials(true);
 
     }
