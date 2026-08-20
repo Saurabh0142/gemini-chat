7f30367dc9059ac4c58f47d6028a1a5bef4cc0a1
Saurabh Yadav <saurabh17again@gmail.com>
2026-08-20 20:10:26 +0530

    Allow frontend CORS requests

--- src/main/java/com/ai/gemini_chat/AIController.java
+++ src/main/java/com/ai/gemini_chat/AIController.java
@@ -2,16 +2,14 @@ package com.ai.gemini_chat;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/qna")
+@CrossOrigin(origins = "https://gemini-chat-mxsh.onrender.com")
 public class AIController {
 
     private final QnAService qnAService;
